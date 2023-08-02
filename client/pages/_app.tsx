c45e224223ee0a6f270e979cea8d91799a9c2147
nokopo-2231 <nokojolemi87gmail.com>
2023-08-02 10:32:09 +0900

    テスト用マンダラ

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+// import '../styles/globals.css';
+// import type { AppProps } from 'next/app';
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+
+
+
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app'; // AppProps型をインポート
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
