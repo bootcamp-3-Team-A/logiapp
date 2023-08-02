ab521dbbddb29ab9fa05ca70448141d95659308b
tomomiK <t0m0m1k221@gmail.com>
2023-08-02 11:06:50 +0900

    feat:router周りを実装した

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from api.openai_api import router as chat_router
 
 app = FastAPI()
+app.include_router(chat_router)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Hello World!"}
