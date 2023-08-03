6cb992071acbef3a72f706685cc6b7ea6a3bd36f
tomomiK <t0m0m1k221@gmail.com>
2023-08-03 11:15:22 +0900

    feat:マンダラチャートにopenai_apiのレスポンスが表示されるようにした

--- server/api/openai_api.py
+++ server/api/openai_api.py
@@ -4,6 +4,8 @@ import os
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
@@ -13,19 +15,34 @@ router = APIRouter()
 
 llm = OpenAI(temperature=0.7)
 
+origins = [
+    "http://localhost:3000",  # Update this with your Next.js frontend URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 特定のオリジンを許可する場合は origins に適切なオリジンを指定
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class ProductInput(BaseModel):
+    product: str
+
 
 prompt = PromptTemplate(
     input_variables=["product"],
-    template='チョコレートから連想される言葉をJSON形式で8つ教えてください => {{"1":"甘い", "2":"幸せ", "3":"バレンタイン", "4":"カカオ", "5":"お菓子", "6":"デザート", "7":"ショコラティエ", "8":"ケーキ"}}\n{product}から連想される言葉を8つ教えてください => ',
+    template='チョコレートから連想される言葉をJSON形式で9個教えてください => {{"1":"甘い", "2":"幸せ", "3":"バレンタイン", "4":"カカオ", "5":"お菓子", "6":"デザート", "7":"ショコラティエ", "8":"ケーキ","9":"美味しい"}}\n{product}から連想される言葉を9つ教えてください => ',
 )
-
 
 chain = LLMChain(llm=llm, prompt=prompt)
 
 
 @router.post("/chat")
-async def chat_endpoint(product: str):
-    # Execute LLM Chain
+async def chat_endpoint(product_input: ProductInput):
+    product = product_input.product
     prediction = chain.run(product)
     return {"response": prediction.strip()}
 
