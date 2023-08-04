8061f814f995700b0f32c33794eda6c91093ce8d
nokopo-2231 <nokojolemi87gmail.com>
2023-08-04 09:11:44 +0900

    style:ハンバーガーメニュー追加

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from 'next/app'; // AppProps型をインポート
+import theme from "../styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) { //分割代入 AppProps型でPropsを受け取る
+  // Componentページコンポーネント自体　pagePropsページコンポーネントに渡されるPropsのオブジェクト
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
